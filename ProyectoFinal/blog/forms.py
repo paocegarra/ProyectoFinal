c98765ecec028b506cae9ee956bc0f66fdbc49c5
paocegarra <paocegarra2@gmail.com>
2022-10-11 21:51:26 -0300

    nuevo formulario

---
+++ ProyectoFinal/blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+class ArticuloForm(forms.Form):
+    
+    titulo = forms.CharField(max_length=30)
+    texto = forms.CharField(max_length=1000)
+    fecha = forms.DateField()
+    
+
+class AutorForm(forms.Form):
+    
+    nombre = forms.CharField(max_length=30)
+    apellido = forms.CharField(max_length=30)
+    profesion = forms.CharField(max_length=30)
+    
+    
+class SeccionForm(forms.Form):
+    
+    nombre = forms.CharField(max_length=30)
+    
--- ProyectoFinal/blog/views.py
+++ ProyectoFinal/blog/views.py
@@ -1,5 +1,9 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from blog.models import Articulo
+from blog.forms import ArticuloForm, SeccionForm, AutorForm
 
 # Create your views here.
 
@@ -8,12 +12,35 @@ def mostrar_inicio(request):
 
 
 def procesar_formulario_articulo(request):
-    return render (request,"blog/formulario-articulo.html")
-
+    
+    if request.method == "GET":
+        mi_formulario = ArticuloForm()
+        contexto = {"formulario" : mi_formulario}
+        return render (request,"blog/formulario-articulo.html", context = contexto)
+    
+    if request.method == "POST":
+        
+        mi_formulario = ArticuloForm(request.POST)
+        if mi_formulario.is_valid():
+            datos_ingresados = mi_formulario.cleaned_data
+            nuevo_modelo = Articulo(
+                titulo = datos_ingresados ["titulo"],
+                texto = datos_ingresados ["texto"],
+                fecha = datos_ingresados ["fecha"],    
+            )
+            
+            nuevo_modelo.save()
+            
+        contexto = {"formulario" : mi_formulario}
+        return render (request,"blog/formulario-articulo.html", context = contexto)
 
 def procesar_formulario_seccion(request):
-    return render (request,"blog/formulario-seccion.html")
+    mi_formulario = SeccionForm()
+    contexto = {"formulario" : mi_formulario}
+    return render (request, "blog/formulario-seccion.html", context = contexto)
 
 
 def procesar_formulario_autor(request):
-    return render (request,"blog/formulario-autor.html")
+    mi_formulario = AutorForm()
+    contexto = {"formulario" : mi_formulario}
+    return render (request,"blog/formulario-autor.html", context = contexto)
